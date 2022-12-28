45dd4ddf49dde764570e549d93d4133668623712
Daryl Dudey <daryljdudey@googlemail.com>
2022-12-28 09:57:32 +0000

    Partial work on DIM for collections

--- desktop/Parser/Tokens/ParserDIM.cpp
+++ desktop/Parser/Tokens/ParserDIM.cpp
@@ -1,16 +1,21 @@
 #include "../Parser.h"
 
 std::any Parser::visitDim(DaricParser::DimContext *context) {
-	ParserToken ps = CreateToken(context, ParserTokenType::DIM);
-	auto r = Reference::Create(state, context->IDENTIFIER()->getText());
-	r->SetDataType(std::any_cast<Primitive>(visit(context->type())));
+    ParserToken ps = CreateToken(context, ParserTokenType::DIM);
+    auto r = Reference::Create(state, context->IDENTIFIER()->getText());
 
-	r->SetAsArray();
-	for (size_t i = 0; i < context->expression().size(); i++) {
-		auto pse = std::any_cast<ParserToken>(visit(context->expression(i)));
-		r->GetIndices().push_back(std::move(pse));
-	}
+    if (context->type()) {
+        r->SetDataType(std::any_cast<Primitive>(visit(context->type())));
+        r->SetAsArray();
+        for (size_t i = 0; i < context->expression().size(); i++) {
+            auto pse = std::any_cast<ParserToken>(visit(context->expression(i)));
+            r->GetIndices().push_back(std::move(pse));
+        }
+    } else {
+        // Collection
+        assert(0);
+    }
 
-	ps.reference = r->GetRef();
-	return ps;
+    ps.reference = r->GetRef();
+    return ps;
 }
